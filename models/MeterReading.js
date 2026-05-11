28cfa234080104bb7ba8a4d6edbf937348d49be7
ManinduDeshan55 <maninduj@gmail.com>
2026-05-11 11:31:20 +0530

    added meter reading model, still working on the schema

---
+++ models/MeterReading.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const meterReadingSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: [true, 'User ID is required'],
+    trim: true
+  },
+  month: {
+    type: String,
+    required: [true, 'Month is required'],
+    match: [/^\d{4}-\d{2}$/, 'Month must be in YYYY-MM format']
+  },
+  previousUnits: {
+    type: Number,
+    required: [true, 'Previous meter reading is required'],
+    min: 0
+  },
+  currentUnits: {
+    type: Number,
+    required: [true, 'Current meter reading is required'],
+    min: 0
+  },
+  unitsConsumed: {
+    type: Number
+  },
+  ratePerUnit: {
+    type: Number,
+  },
+  estimatedBill: {
+    type: Number
+  },
+  notes: {
+    type: String,
+    trim: true
+  }
+}, { timestamps: true });
+
+
+meterReadingSchema.pre('save', function (next) {
+  this.unitsConsumed = this.currentUnits - this.previousUnits;
+  this.estimatedBill = this.unitsConsumed * this.ratePerUnit;
+  next();
+});
+
+module.exports = mongoose.model('MeterReading', meterReadingSchema);
